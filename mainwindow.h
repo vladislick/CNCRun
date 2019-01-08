ac9528f16fd58b4446cc7f9d992dd255b38bd930
vladislick <vladislickness@gmail.com>
2019-01-08 22:29:37 +0700

    Update

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 #include <QComboBox>
+#include <QTextEdit>
 #include "conffile.h"
 #include <QTimer>
 #include <QFileDialog>
@@ -24,68 +26,32 @@ public:
 private slots:
     void on_action_exit_triggered();
 
-    void on_action_about_2_triggered();
+    void on_button_clear_clicked();
 
-    void on_action_settings_triggered();
+    void on_button_start_clicked();
+
+    void on_button_com_clicked();
 
     void on_action_open_triggered();
-
-    void on_pushButton_clicked();
-
-    void on_pushButton_4_clicked();
-
-    void console();
-
-    void on_pushButton_3_clicked();
-
-    void on_pushButton_5_clicked();
-
-    void timerX_slot();
-    void timerY_slot();
-    void timerZ_slot();
-
-    void on_pushButton_7_pressed();
-
-    void on_pushButton_7_released();
-
-    void on_pushButton_13_pressed();
-
-    void on_pushButton_13_released();
-
-    void on_pushButton_9_pressed();
-
-    void on_pushButton_9_released();
-
-    void on_pushButton_11_pressed();
-
-    void on_pushButton_11_released();
 
 private:
     Ui::MainWindow *ui;
 
-    void portsUpdate(QSerialPortInfo*, QComboBox*);
-    bool portsAvailable(QSerialPortInfo*);
-    void status(short);
+    ///Отправить текст на консоль вывода
+    void consoleWrite(QString, QTextEdit*);
+    ///Отправить текст в порт
     short serialWrite(QSerialPort*, const char*, int);
-
-    //Координаты
-    short axisX = 0;
-    short axisY = 0;
-    short axisZ = 0;
-    //Ускорение
-    short dx = 0;
-    short dy = 0;
-    short dz = 0;
+    ///Обновляет данные о портах
+    void serialUpdate(QSerialPortInfo*, QComboBox*);
+    ///Возвращает количество доступных портов
+    int serialAvailable(QSerialPortInfo*);
+    ///Обрабатывает открытие файла
+    void fileOpen(QString);
 
     //Последовательный порт
     QSerialPort     *comPort;
     QSerialPortInfo *portInfo;
-    QFileDialog     *fileDialog;
-    ConfFile        *file;
-    QTimer          *timerX;
-    QTimer          *timerY;
-    QTimer          *timerZ;
-    QString         *gcode;
+    ConfFile        *conffile;
 };
 
 #endif // MAINWINDOW_H
